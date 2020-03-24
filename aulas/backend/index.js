a9ad54e713aad9137f76dcad8cbec9a40a9c736b
JeanFSorio <jeanfsorio@gmail.com>
2020-03-24 12:07:33 -0300

    Primeiro dia do curso - conhecendo o curso

---
+++ aulas/backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app =express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        evento: "Semana Omnistack 11",
+        aluno: "Jean Felipe"
+    });
+
+});
+
+app.listen(3333);
